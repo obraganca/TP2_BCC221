aa7ef9e065d47056d03c6d72721ef9cc8b77b67c
marcantonioo <dinizsilvam3@gmail.com>
2025-08-15 18:41:09 -0300

    Correção: palavras do jogo agora sao aleatorias

--- src/main/java/com/termo/model/DataSourceModel.java
+++ src/main/java/com/termo/model/DataSourceModel.java
@@ -3,13 +3,16 @@ package com.termo.model;
 import com.termo.gui.GameWindow;
 
 import javax.xml.crypto.Data;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Vector;
+import java.nio.file.Files;
+
+import static java.lang.System.in;
+
 
 public class DataSourceModel {
     private String filename = "datasource.txt";
@@ -21,15 +24,17 @@ public class DataSourceModel {
 
 
     public String processingData(){
-        try {
-            File myFile = new File(DataSourceModel.class.getResource("/"+filename).toURI());
-            RandomAccessFile data = new RandomAccessFile(myFile, "r");
+        try (InputStream in = DataSourceModel.class.getResourceAsStream("/" + filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
 
-            Random posicaoaleatoria = new Random();
-            data.seek(posicaoaleatoria.nextInt((int)data.length()));
-            String text = data.readLine();
-            data.close();
-            return text;
+                Vector<String> palavras = new Vector<>();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    palavras.add(line);
+                }
+
+                Random random = new Random();
+                return palavras.get(random.nextInt(palavras.size()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

192f5d94d004905755f6cf8ae835b5112a71a9a8
Thayllon Bragança <thayllonbraganca@gmail.com>
2025-08-21 19:45:07 -0300

    fix: fixing datasource

--- src/main/java/com/termo/TermoApp.java
+++ src/main/java/com/termo/TermoApp.java
@@ -4,7 +4,7 @@ import com.termo.gui.GameWindow;
 
 public class TermoApp {
     public static void main(String[] args) {
-        Login.setFilePath("/home/obraganca/Documents/TP2_BCC221/src/main/resources/datasource.txt");
+        Login.setFilePath(args[2]);
         javax.swing.SwingUtilities.invokeLater(() -> {
             new GameWindow().showEventDemo();
         });
